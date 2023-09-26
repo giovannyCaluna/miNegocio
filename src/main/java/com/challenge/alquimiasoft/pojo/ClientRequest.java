83bab0e40dd854af51e7be1f4828fe563e57cb75
giovannyCaluna <giiovhanny@gmail.com>
2023-09-26 22:21:17 +0200

    Finishing requeriments

---
+++ src/main/java/com/challenge/alquimiasoft/pojo/ClientRequest.java
@@ -0,0 +1,2 @@
+package com.challenge.alquimiasoft.pojo;public class ClientRequest {
+}
---
+++ src/main/java/com/challenge/alquimiasoft/pojo/UserIdentificationRequest.java
@@ -0,0 +1,22 @@
+package com.challenge.alquimiasoft.model;
+
+public class UserIdentificationRequest {
+    private String name;
+    private String identification;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+}
